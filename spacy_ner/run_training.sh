cddbdbecfb598346ac22283e1e39a06b96e81d79
Florian Borchert <Florian.Borchert@hpi.de>
2024-02-02 16:03:30 +0100

    NER Short Spans

--- spacy_ner/run_training.sh
+++ spacy_ner/run_training.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 TIMESTAMP=`date +%Y-%m-%d_%H-%M-%S`
 
-if [[ $# -ne 3 ]] ; then
-  echo 'Usage: run_training.sh <cuda> <data_path> <model>'
+if [[ $# -ne 4 ]] ; then
+  echo 'Usage: run_training.sh <cuda> <data_path> <model> <config>'
   exit 1
 fi
 
 cuda="$1"
 data_path="$2"
 model="$3"
+config="$4"
 
-python -m spacy project run train . --vars.gpu "$cuda" --vars.run_name "$TIMESTAMP-cuda-$cuda" --vars.data_path "$data_path" --vars.model "$model"
+python -m spacy project run train . --vars.gpu "$cuda" --vars.run_name "$TIMESTAMP-cuda-$cuda" --vars.data_path "$data_path" --vars.model "$model" --vars.config "$config"
